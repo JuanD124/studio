d2c6293d801b53731616baaae410aea078f964f9
Juan Diego Castañeda León <juandiego.u703@gmail.com>
2025-07-18 02:29:23 +0000

    igualmente el del icono de la pagina

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 24 24%22 fill=%22none%22 stroke=%22currentColor%22 stroke-width=%222%22 stroke-linecap=%22round%22 stroke-linejoin=%22round%22><path d=%22M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z%22 /><path d=%22M12 12a5 5 0 0 1 5 5%22 /><path d=%22M12 12a5 5 0 0 0-5 5%22 /><path d=%22M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z%22 /><line x1=%2216%22 y1=%226%22 x2=%2216.01%22 y2=%226%22 /><line x1=%2219%22 y1=%228%22 x2=%2219.01%22 y2=%228%22 /></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
